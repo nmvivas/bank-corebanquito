737661d7b92c51d2ee81e6c72d79ad82d84b6363
Charles Zambrano <charleszambranoespe@outlook.com>
2024-06-13 10:39:17 -0500

    [UPDATE] Random password generation

--- src/main/java/com/banquito/core/bank/service/BankUserService.java
+++ src/main/java/com/banquito/core/bank/service/BankUserService.java
@@ -1,5 +1,6 @@
 package com.banquito.core.bank.service;
 
+import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +25,9 @@ public class BankUserService {
     private final BankUserRepository repository;
     private final BankRepository bankRepository;
     private final BankUserMapper bankUserMapper;
+    private final SecureRandom random = new SecureRandom();
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int PASSWORD_LENGTH = 10;
 
     public BankUserService(BankUserRepository repository, BankRepository bankRepository,
             BankUserMapper bankUserMapper) {
@@ -68,13 +72,14 @@ public class BankUserService {
             throw new RuntimeException("Email repetido");
         }
         BankUser user = this.bankUserMapper.toPersistence(dto);
-        Bank bank = this.bankRepository.findAll().getFirst();
+        Bank bank = this.bankRepository.findAll().get(0);
         user.setCodeBank(bank.getCode());
         user.setState("BLO");
         user.setTypeUser("TEL");
         user.setCreationDate(LocalDateTime.now());
-        // TODO: Generacion de password
-        user.setPassword("CambiarClave1");
+        // Generar y encriptar password
+        String generatedPassword = generateRandomPassword();
+        user.setPassword(DigestUtils.md5Hex(generatedPassword));
         BankUser userCreated = this.repository.save(user);
         return this.bankUserMapper.toDTO(userCreated);
     }
@@ -97,11 +102,18 @@ public class BankUserService {
         if (user == null) {
             throw new RuntimeException("No existe el usuario: " + userName);
         }
-        // TODO: Generar clave
-        String password = "GenerarClave2";
-        String md5Hex = DigestUtils.md5Hex(password);
+        // Generar y encriptar clave
+        String generatedPassword = generateRandomPassword();
+        String md5Hex = DigestUtils.md5Hex(generatedPassword);
         user.setPassword(md5Hex);
         this.repository.save(user);
     }
 
+    private String generateRandomPassword() {
+        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            password.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return password.toString();
+    }
 }
